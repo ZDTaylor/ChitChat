8e2243b5b23559e4e48ecf125c9079c251b3cf94
Unknown <zdt1997@gmail.com>
2018-04-04 00:28:18 -0500

    Change required js imports

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <!--[if lt IE 7]>      <html lang="en" ng-app="myApp" class="no-js lt-ie9 lt-ie8 lt-ie7 full-height"> <![endif]-->
 <!--[if IE 7]>         <html lang="en" ng-app="myApp" class="no-js lt-ie9 lt-ie8 full-height"> <![endif]-->
 <!--[if IE 8]>         <html lang="en" ng-app="myApp" class="no-js lt-ie9 full-height"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="en" ng-app="myApp" class="no-js full-height"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" ng-app="app" class="no-js full-height"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,7 +36,7 @@
     <![endif]-->
 
     <!-- Navbar for the page -->
-    <nav class="navbar navbar-default navbar-fixed-top" ng-controller="NavCtrl">
+    <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
 
             <!-- Chit Chat icon -->
@@ -46,7 +46,7 @@
                 </a>
             </div>
 
-            <p class="navbar-text text-center"> {{headerMessage}} </p>
+            <p class="navbar-text text-center"></p>
 
             <!-- Sign in button -->
             <button type="button" class="btn btn-default navbar-btn navbar-right">Sign in</button>
@@ -54,11 +54,11 @@
     </nav>
 
     <!-- Main body of the chat application -->
-    <div class="container full-height" ng-controller="ChatCtrl">
+    <div class="container full-height">
         <div class="row full-height">
             <div class="col-xs-12 full-height">
                 <div class="well full-height chat-window">
-                    <p class="text-center"> {{bodyMessage}} </p>
+                    <p class="text-center"></p>
                 </div>
             </div>
         </div>
@@ -68,21 +68,31 @@
 
     <!-- Comment the following before final upload to reduce file size -->
     <script src="js/vendor/angular.js"></script>
+    <script src="js/vendor/angular-resource.js"></script>
     <script src="js/vendor/angular-animate.js"></script>
     <script src="js/vendor/ui-bootstrap-tpls-2.5.0.js"></script>
 
     <!-- Unomment the following before final upload to reduce file size -->
     <!--
     <script src="js/vendor/angular.min.js"></script>
+    <script src="js/vendor/angular-resource.js"></script>
     <script src="js/vendor/angular-animate.min.js"></script>
     <script src="js/vendor/ui-bootstrap-tpls-2.5.0.min.js"></script>
     -->
 
     <!-- Custom angular app code for Chit Chat
-         app.js must be loaded before all other controllers
+         app.module.js must be loaded after other app modules.
+         app modules must be loaded before any services that depend on them.
     -->
-    <script src="js/app.js"></script>
-    <script src="js/app.navCtrl.js"></script>
-    <script src="js/app.chatCtrl.js"></script>
+    <script src="js/users.module.js"></script>
+    <script src="js/user.service.js"></script>
+
+    <script src="js/messages.module.js"></script>
+    <script src="js/message.service.js"></script>
+
+    <script src="js/app.module.js"></script>
+    <script src="js/constants.js"></script>
+
+    <script src="js/test.controller.js"></script>
 </body>
 </html>
