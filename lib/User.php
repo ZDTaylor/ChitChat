5c2746abf1dbfbc9b86cdf2ec75ce6d891429bab
Unknown <anubo16@gmail.com>
2018-04-06 15:54:45 -0500

    Completed User class

--- lib/User.php
+++ lib/User.php
@@ -1,7 +1,23 @@
 <?php
 
 class User {
+    public $userID;
+    public $displayName;
+    public $isAdmin;
+    public $email;
+    public $password;
+    public $suspended;
+    public $banned;
 
+    public function User(){
+        $this -> userID = 0;
+        $this -> displayName = "";        
+        $this -> isAdmin = false;
+        $this -> email = "";
+        $this -> password = "";
+        $this -> suspended = "";
+        $this -> banned = false;
+    }
 }
 
 ?>
