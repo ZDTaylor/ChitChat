e92d1282e3593e0ddd2eb0727e308c4dd5b01139
Unknown <zdt1997@gmail.com>
2018-04-16 15:42:18 -0500

    Update Post functionaity

--- api/post.php
+++ api/post.php
@@ -9,19 +9,37 @@
         "success" => false
     ];
 
+    session_start();
+    if (!empty($_SESSION["user"])) {
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $data = json_decode(file_get_contents("php://input"), true);
+            $data = json_decode(file_get_contents("php://input"), true);
 
-        if (!empty($data["message"])) {
+            if (!empty($data["message"])) {
 
-            // Create a message object, and update the required fields with the same fields from $data["message"]
-            // attempt to post the message and update $response["success"] accordingly
-            $Messenger->post($message);
+                // Create a message object, and update the required fields with the same fields from $data["message"]
+                // attempt to post the message and update $response["success"] accordingly
+                $message = new Message();
+                $message->poster = $_SESSION["user"]->userID;
+                $message->content = $data["message"]->content;
+                $message->mentions = $data["message"]->mentions;
+
+                $message->mentions = array_unique($message->mentions);
+
+                if (!empty($message->content)) {
+                    $post = $Messenger->post($message);
+
+                    if ($post === true) {
+                        $response["success"] = true;
+                    }
+                }
+
+            }
+
         }
-
     }
+
 
     echo json_encode($response);
 ?>
